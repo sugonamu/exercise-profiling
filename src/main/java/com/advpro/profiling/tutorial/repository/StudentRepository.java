874c77d29ec1c20a6cb645261d10bdb996f06523
sugonamu <je0jwff@gmail.com>
2025-03-14 01:38:08 +0700

    [Refactoring] - Optimizing method findStudentWithHighestGpa and joinStudentNames

--- src/main/java/com/advpro/profiling/tutorial/repository/StudentRepository.java
+++ src/main/java/com/advpro/profiling/tutorial/repository/StudentRepository.java
@@ -3,10 +3,17 @@ package com.advpro.profiling.tutorial.repository;
 import com.advpro.profiling.tutorial.model.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Query;
 
 /**
  * @author muhammad.khadafi
  */
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
+    // Get the student with the highest GPA
+    Student findTopByOrderByGpaDesc();
+
+    // Aggregate student names using Postgres' STRING_AGG native function
+    @Query(value = "SELECT STRING_AGG(name, ', ') FROM students", nativeQuery = true)
+    String joinStudentNamesAggregate();
 }
